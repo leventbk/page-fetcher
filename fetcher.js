8fcc067bf476d1f69655d8aad53aa71c5250ad31
leventkarakus <leventbkarakus@hotmail.com>
2023-05-11 18:24:05 -0400

    eslinted

--- fetcher.js
+++ fetcher.js
@@ -5,27 +5,27 @@
 const request = require('request');
 const fs = require('fs');
 const url = process.argv[2];
-const localPath =process.argv[3];
+const localPath = process.argv[3];
 
-const fetchAndSave = function (url, localPath) {
-	request(url, (error, response, body) => {
-		if (error) {
-			console.log('Oops! Failed to download resouce: ', error);
-			return;
-		}
-		fs.writeFile(localPath, body, (error) => {
-			if(error) {
-				console.log('Dude! is localPath has an issue!:', localPath)
-			} else {
-				console.log(`Awesome! Download size: ${body.length} Doc.address: ${localPath}`)
-			}
-		})
-	})
-}
+const fetchAndSave = function(url, localPath) {
+  request(url, (error, response, body) => {
+    if (error) {
+      console.log('Oops! Failed to download resouce: ', error);
+      return;
+    }
+    fs.writeFile(localPath, body, (error) => {
+      if (error) {
+        console.log('Dude! is localPath has an issue!:', localPath);
+      } else {
+        console.log(`Awesome! Download size: ${body.length} Doc.address: ${localPath}`);
+      }
+    });
+  });
+};
 
 if (!url || !localPath) {
-	console.log('Dude! please enter both parameters');
-	console.log(`like; node fetcher.js <url> <local-path>`);
+  console.log('Dude! please enter both parameters');
+  console.log(`like; node fetcher.js <url> <local-path>`);
 } else {
-	fetchAndSave(url, localPath);
+  fetchAndSave(url, localPath);
 }
